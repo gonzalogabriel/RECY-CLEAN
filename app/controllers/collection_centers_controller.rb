715a102eba3590326a94912293a7b990b26a8bf8
Javier Pacheco <javipacl@gmail.com>
2018-09-26 15:17:41 -0500

    Pelaos miren

--- app/controllers/collection_centers_controller.rb
+++ app/controllers/collection_centers_controller.rb
@@ -1,2 +1,6 @@
 class CollectionCentersController < ApplicationController
+    def index
+#       autor = autor.find(params [:autor_id])
+#       render json: autor.books,    status: :ok 
+    end    
 end
