e4c61f9c71d4ff0f7bb1838754d9cbc790698bb8
Javier Pacheco <javipacl@gmail.com>
2018-09-26 15:31:21 -0500

    Comentarié el index en Collection_center_Controlles

--- app/controllers/collection_centers_controller.rb
+++ app/controllers/collection_centers_controller.rb
@@ -1,6 +1,6 @@
 class CollectionCentersController < ApplicationController
-    def index
+#    def index
 #       autor = autor.find(params [:autor_id])
 #       render json: autor.books,    status: :ok 
-    end    
+#    end    
 end
