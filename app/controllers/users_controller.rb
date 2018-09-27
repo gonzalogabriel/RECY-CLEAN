ea56c5724568fba6c325a1cafa31396680f185fa
Javier Pacheco <javipacl@gmail.com>
2018-09-27 17:10:20 -0500

    Create pacheco

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,15 @@ class UsersController < ApplicationController
         @users = User.all
         puts @users
         render json: @users, status: :ok
-
     end
 
+    def create
+        @user = User.create(params.permit(:document, :name, :surname, :account_contract, :birth_date, :email, :movil, :username, :password, :date_reg))
+        puts "Ok"
+        puts params
+        puts "Ok"
+        render json: @user, status: :created
+        
+    end 
 end
+   
