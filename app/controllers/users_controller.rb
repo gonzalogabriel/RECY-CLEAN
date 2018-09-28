44334c1d45b2a3b41921a745c9170a70209bc825
Javier Pacheco <javipacl@gmail.com>
2018-09-28 15:22:24 -0500

    Users Completo - Javipacl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,34 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(params.permit(:document, :name, :surname, :account_contract, :birth_date, :email, :movil, :username, :password, :date_reg))
+        @user = User.create(user_params)
         puts "Ok"
         puts params
         puts "Ok"
         render json: @user, status: :created
-        
     end 
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user, status: :ok
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        render json:@user, status: :upgrated  
+    end
+    
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json:@user, status: :destroyed
+    end
+
+    def user_params
+        params.permit(:document, :name, :surname, :account_contract, :birth_date, :email, :movil, :username, :password, :date_reg)
+    end
+
+
 end
    
