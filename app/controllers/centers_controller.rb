a50dcc741f019f783a6c561b57e1b296c6ef6770
Gonzalo Gonzalez <gonzgonc@gmail.com>
2018-10-01 18:02:40 -0500

    Se deja listo los metodos de lso Centros de Acopio

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -1,24 +1,43 @@
 class CentersController < ApplicationController
   
-  
-  
-  
+    def index
+        @listar_centros = Center.all
+        puts @listar_centros
+        render json: @listar_centros, status: :ok
+    end
+    
+    def create
+        @creaCentro = Center.create(user_params)
+        
+        render json: @creaCentro, status: :created
+    end 
+    
+    def user_params
+        params.permit(:name_center, :address_center, :gps_latitude, :gps_longitude, :email_center, :phone_center, :schedule)
+    end
+    
+    def show
+        @Seleccionar_Centro = Center.find(params[:id])
+        render json: @Seleccionar_Centro, status: :ok
+    end
+
+    def update
+        @actualizar_centro = Center.find(params[:id])
+        @actualizar_centro.update(user_params)
+        render json: @actualizar_centro, status: :actualizado  
+    end
+
+    def destroy
+        @Borrar_Centro = Center.find(params[:id])
+        @Borrar_Centro.destroy
+        
+        render json: @Borrar_Centro, status: :Borrado
+    end
+
+
+
+
 end
 
 
-# Información de APP biblioteca de William 
 
-#def index
-#       autor = autor.find(params [:autor_id])
-#       render json: autor.books,    status: :ok 
-end
-
-#def create
-#    book.new
-#    author = autor.find(param[:author_id])
-#    book.autor = author
-#    book.name = params[:name]
-#    book.code = params[:code]
-#    book.save
-#    render json: book, status: :ok
-#end
